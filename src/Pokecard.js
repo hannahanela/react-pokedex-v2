0baa5d49b17f5f47f353527f8dc25c65f94bbcc7
anela <anela89@gmail.com>
2022-10-24 14:58:39 -0700

    Initial components for pokedex.

---
+++ src/Pokecard.js
@@ -0,0 +1,11 @@
+const IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+
+function Pokecard() {
+    return (
+        <div className="Pokecard">
+
+        </div>
+    )
+}
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,11 @@
+import Pokecard from "./Pokecard
+
+function Pokedex() {
+    return (
+        <div className="Pokedex">
+            TODO: map over pokemon
+        </div>
+    );
+}
+
+export default Pokedex;
